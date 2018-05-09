9c1641e06acd9c9beafa9f2f2c193a93df3ae8d2
DanFein <danfeinstat@gmail.com>
2018-05-09 12:16:50 -0700

    Search routes added, server.js imports routes, config updated

---
+++ routes/search-routes.js
@@ -0,0 +1,168 @@
+//import models
+const db = require("../models");
+// const express = require("express");
+
+//Routes
+
+//DEV NOTE
+//Table Names and Table Keys are currently PLACEHOLDERS
+//They need to be updated for actual use
+//Same is true of Handlebars Page Names
+//Testing instructions and test post routes are at the bottom of the page
+
+//root route
+//find featured
+//switched from finding featured to finding all
+//switch is to support auto-complete npm package.
+//code determining featured and tag list relegated to front-end
+
+module.exports = function(app) {
+  //root route
+  app.get("/", function(req, res) {
+    db.Video.findAll({
+      include: [db.Contributor],
+    }).then(function(result) {
+      let hbsObject = {
+        videos: result,
+      };
+      console.log(hbsObject);
+      //update with correct handlebars link
+      res.render("index", hbsObject);
+    });
+  });
+
+  //Search Routes
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  //find all videos
+  //include user list
+  app.get("/api/videos", function(req, res) {
+    db.Video.findAll({
+      include: [db.Contributor],
+    }).then(function(result) {
+      // res.json(result);
+      let hbsObject = {
+        videos: result,
+      };
+      //   console.log(hbsObject);
+      //update with correct handlebars link
+      res.render("search_results", hbsObject);
+      // res.json(result);
+    });
+  });
+
+  //find all videos with a given title
+  app.get("/api/videos/:title", function(req, res) {
+    db.Video.findAll({
+      where: {
+        title: req.params.title,
+      },
+      include: [db.Contributor],
+    }).then(function(result) {
+      let hbsObject = {
+        videos: result,
+      };
+      // console.log(result);
+      //update with correct handlebars link
+      res.render("search_results", hbsObject);
+      // res.json(result);
+    });
+  });
+
+  //find all videos with a given author
+  //include video list
+  app.get("/api/videos/:author", function(req, res) {
+    db.Contributor.findAll({
+      where: {
+        user_name: req.params.author,
+      },
+      include: [db.Video],
+    }).then(function(result) {
+      let hbsObject = {
+        videos: result,
+      };
+      //   console.log(hbsObject);
+      //update with correct handlebars link
+      res.render("search_results", hbsObject);
+      // res.json(result);
+    });
+  });
+
+  //find all videos with a given tag
+  app.get("/api/videos/:keyword", function(req, res) {
+    db.Video.findAll({
+      where: {
+        tag: req.params.keyword,
+      },
+      include: [db.Contributor],
+    }).then(function(result) {
+      let hbsObject = {
+        videos: result,
+      };
+      //   console.log(hbsObject);
+      //update with correct handlebars link
+      res.render("search_results", hbsObject);
+      // res.json(result);
+    });
+  });
+
+  //Contributor registration route
+  //add data set if needed
+  //change handlebars file name
+  app.get("/register", function(req, res) {
+    res.render("user_login");
+  });
+
+  //Contributor login route
+  //add data set if needed
+  //change handlebars file name
+
+  app.get("/login", function(req, res) {
+    res.render("user_login");
+  });
+  //User login route
+
+  //Post Routes
+  //will be taken care of once we decide how to handle
+  //authentication
+
+  //post routes for testing
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //HOW TO TEST A ROUTE
+  //comment out all active code in the .then of the route to test
+  //uncomment res.json(result);
+  //Use postman to post to contributor, then
+  //use postman to post to videos.
+  //Order matters here or the foreign key will be null.
+  //Use postman to test a given search route.
+  //If you're using nodemon and you save a change,
+  //you will have to run the two post routes again before testing.
+
+  app.post("/api/contributor", function(req, res) {
+    db.Contributor.create({
+      name: "test2guy",
+      specialty: "dirt",
+      title: "Dr.",
+    }).then(function(newCont) {
+      res.json(newCont);
+    });
+  });
+
+  app.post("/api/videos", function(req, res) {
+    db.Video.create({
+      title: "movieTitleTest",
+      description: "this is a video test",
+      link: "fakelinkhere",
+      category: "medical",
+      keywordOne: "eyballs",
+      keywordTwo: "tatas",
+      keywordThree: "boogaboo",
+      ContributorId: 1,
+    }).then(function(newVideo) {
+      res.json(newVideo);
+    });
+  });
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  //export function ends
+};
--- server.js
+++ server.js
@@ -35,7 +35,7 @@ db.sequelize
   });
 // Routes
 // =============================================================
-
+require("./routes/search-routes.js")(app);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
