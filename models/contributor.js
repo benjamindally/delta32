ba2bc8e2c3626b7c1011e00c27b74e660c4d976c
Benjamin Dally <benjamin.dally@icloud.com>
2018-05-08 21:09:06 -0700

    added models

---
+++ models/contributor.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+  //craetes a "Contributor" model that will match to db
+  var Contributor = sequelize.define("Contributor", {
+    //the name of the contibutor in text form
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [1] },
+    },
+    //the specialty if any of the contributor
+    specialty: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    //the title of the contributor
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [1] },
+    },
+  });
+
+  Contributor.associate = function(models) {
+    Contributor.hasMany(models.Video, {
+      onDelete: "cascade",
+    });
+  };
+
+  return Contributor;
+};
---
+++ models/video.js
@@ -0,0 +1,53 @@
+module.exports = function(sequelize, DataTypes) {
+  var Video = sequelize.define("Video", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [1] },
+    },
+
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [3, 140] },
+    },
+
+    link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [1] },
+    },
+
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [1] },
+    },
+
+    keywordOne: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [1] },
+    },
+
+    keywordTwo: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    keywordThree: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  });
+
+  Video.associate = function(models) {
+    Video.belongsTo(models.Contributor, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
+
+  return Video;
+};
