83c3620f5a17ec0600c8274821788044a83e0885
Benjamin Dally <benjamin.dally@icloud.com>
2018-05-16 23:03:39 -0700

    meh?

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ var basename = path.basename(__filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
-
+var sequelize = "";
 // if (config.use_env_variable) {
 //   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 // } else {
